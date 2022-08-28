69dfef45ef5a2eed4eeaa7a0e88863bb3486a508
avongalie <alexsiaavila@gmail.com>
2022-08-27 21:54:02 -0400

    Completed PreWork
    all prework including bonus features

---
+++ Prework/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Prework
+//
+//  Created by Alexsia Avila on 8/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        TextLabel.textColor = UIColor.black
+        view.backgroundColor = UIColor.systemYellow
+        
+    }
+
+    @IBAction func TextFieldClicked(_ sender: Any) {
+    }
+    @IBOutlet weak var TextField: UITextField!
+    @IBOutlet var BackgroundView: UIView!
+    @IBOutlet weak var TextLabel: UILabel!
+    @IBAction func ChangeTextColorClicked(_ sender: Any) {
+        TextLabel.textColor = UIColor.purple
+    }
+
+    @IBAction func ChangeViewColorClicked(_ sender: Any) {
+        view.backgroundColor = UIColor.cyan
+    }
+    @IBAction func ChangeTextClicked(_ sender: Any) {
+        if TextField.text == "" {
+            TextLabel.text = "Hello"
+        }else{
+            TextLabel.text = TextField.text
+        }
+        view.endEditing(true)
+    }
+}
+
